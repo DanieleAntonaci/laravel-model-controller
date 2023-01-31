127f2df0cc8548477ea773a234aaea4b2907b8af
DanieleAntonaci <danieleanto96@gmail.com>
2023-01-31 16:58:01 +0100

    percorsi per ottenere dati dal db

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\movies;
 
 class MainController extends Controller
 {
     public function index() {
-
+        $movies= movies::all();
+        $data= [
+            'movies'=> $movies
+        ];
         return view('pages.app');
     }
 }
--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -1,2 +1,8 @@
 @extends('layouts.main-layout')
 
+@section('name-film')
+    <h1>name films</h1>
+    @foreach ($movies as $movie)
+        {{$movie-> title}}
+    @endforeach
+@endsection
